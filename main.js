31027abba4d3009384d3bda14b20b41d596e5bcf
Maria Eduarda <eduarda.facio@estudante.ufjf.br>
2023-05-30 19:27:54 -0300

    add estado base

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
 
+let estado = 0;
+
 function envia(e){
     e.preventDefault();
     alert('Formulário Enviado!');
+    estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
 }
